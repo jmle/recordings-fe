a5c5ea5a6fe206aec5b976abb49567449610ee6e
Juanma <lefletj@ryanair.com>
2019-07-28 20:51:47 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    state = {
+        recordings: []
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:8080/recordings')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({recordings: data._embedded.recordings})
+            })
+            .catch(console.log)
+    }
+
+    render() {
+        return (
+            <div className="container recordings-engine">
+                <h1>Recordings engine</h1>
+                <div className="container search-form">
+                    <SearchForm/>
+                </div>
+                <div className="container">
+                    <Recordings recordings={this.state.recordings}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class SearchForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        fetch('http://localhost:8080/recordings' + SearchForm.getSearchString(formData));
+    }
+
+    static getSearchString = formData => {
+        let str = "?";
+        if (formData.get("beat")) str = str.concat("beat=").concat(encodeURIComponent(formData.get("beat")));
+        if (formData.get("tempo")) str = str.concat("tempo=").concat(formData.get("tempo"));
+        return str;
+    };
+
+    render() {
+        return (
+            <form id="jandepora" onSubmit={this.handleSubmit}>
+                <div className="row">
+                    <div className="form-group col">
+                        <label htmlFor="tempoInput">Tempo</label>
+                        <input type="search" name="tempo" className="form-control" id="tempoInput"
+                               placeholder="Filter by tempo"/>
+                    </div>
+                    <div className="form-group col">
+                        <label htmlFor="beatInput">Beat</label>
+                        <input type="search" name="beat" className="form-control" id="beatInput"
+                               placeholder="Filter by beat"/>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary">Search</button>
+            </form>
+        )
+    }
+}
+
+const Recordings = ({recordings}) => {
+    return (
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Filename</th>
+                <th scope="col">Date</th>
+                <th scope="col">Tempo</th>
+                <th scope="col">Beat</th>
+                <th scope="col">Style</th>
+                <th scope="col">Comments</th>
+            </tr>
+            </thead>
+            <tbody>
+            {recordings.map((recording) => (
+                <Recording recording={recording}/>
+            ))}
+            </tbody>
+        </table>
+    )
+};
+
+const Recording = ({recording}) => {
+    return (
+        <tr>
+            <th scope="row">{recording.filename}</th>
+            <td>{recording.date}</td>
+            <td>{recording.tempo}</td>
+            <td>{recording.beat}</td>
+            <td>{recording.style}</td>
+            <td>{recording.comments}</td>
+        </tr>
+    )
+};
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
